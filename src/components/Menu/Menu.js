eb08831cfd04f3c73af899d64de37ff1ceb00ccf
Thomas Lowry <minilao94@yahoo.com>
2017-12-09 16:24:10 -0700

    done with menu styles

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -6,30 +6,33 @@ export default function Menu(props) {
     console.log(props.section)
     return (
         <div id="Menu" className={props.open ? "open" : "closed"}>
+
+
+
             <Link to="/" className={props.section === '/landing' ? 'nav-button selected' : 'nav-button'} onClick={props.toggleMenu} >
                 <div className="slide slide-left" />
                 <div className="slide slide-right" />
-                Home
+                <div className="nav-text">Home</div>
             </Link>
             <Link to="/about" className={props.section === '/about' ? 'nav-button selected' : 'nav-button'} onClick={props.toggleMenu} >
                 <div className="slide slide-left" />
                 <div className="slide slide-right" />
-                About
+                <div className="nav-text">About</div>
             </Link>
             <Link to="/skills" className={props.section === '/skills' ? 'nav-button selected' : 'nav-button'} onClick={props.toggleMenu} >
                 <div className="slide slide-left" />
                 <div className="slide slide-right" />
-                Skills
+                <div className="nav-text">Skills</div>
             </Link>
             <Link to="/projects" className={props.section === '/projects' ? 'nav-button selected' : 'nav-button'} onClick={props.toggleMenu} >
                 <div className="slide slide-left" />
                 <div className="slide slide-right" />
-                Projects
+                <div className="nav-text">Projects</div>
             </Link>
             <Link to="/contact" className={props.section === '/contact' ? 'nav-button selected' : 'nav-button'} onClick={props.toggleMenu} >
                 <div className="slide slide-left" />
                 <div className="slide slide-right" />
-                Contact
+                <div className="nav-text">Contact</div>
             </Link>
         </div>
     )
